3ca12c6e29227f99174a4ffbb5ee214d0de7b7de
Chirag <20chiragsharma04@gmail.com>
2026-07-24 23:28:45 +0530

    Data Into MongoDB

---
+++ push_data.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+
+import certifi
+ca = certifi.where() #ca = certificate authority
+
+import pandas as pd
+import numpy as np
+import pymongo
+from src.exception import NetworkSecurityException
+from src.logger import logging
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    def csv_to_json_convertor(self,file_path):
+        try:
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records = data.to_dict(orient="records")
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def insert_to_mongo(self,records,database,collection):
+        try:
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return len(self.records)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+if __name__ == "__main__":
+    FILE_PATH = r"Network_Data\phisingData.csv"
+    DATABASE = "ChiragAI"
+    collection = "NetworkData"
+    networkobj = NetworkDataExtract()
+    records = networkobj.csv_to_json_convertor(file_path=FILE_PATH)
+    print(records)
+    no_of_records = networkobj.insert_to_mongo(records,DATABASE,collection) 
+    print(no_of_records)
--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from scr.logging import logger
+from src import logger
 class NetworkSecurityException(Exception):
     def __init__(self,error_message,error_details:sys):
         self.error_message = error_message
