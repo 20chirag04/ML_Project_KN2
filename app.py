a05d49c1c6a36cb967cfdd660c00edf0dc8ca86a
Chirag <20chiragsharma04@gmail.com>
2026-07-30 01:53:07 +0530

    Pipeline and Model Training is completed

--- app.py
+++ app.py
@@ -87,3 +87,6 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
     
 if __name__=="__main__":
     app_run(app,host="0.0.0.0",port=8000)
+
+
+## To run on your device: uvicorn app:app --reload
